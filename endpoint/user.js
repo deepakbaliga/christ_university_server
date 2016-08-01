59e261cb4a6c84506f283af0a16a65430212c0b7
Deepak Baliga <reachme@deepakbaliga.com>
2016-08-01 11:06:09 +0530

    Added mongoose framework

--- endpoint/user.js
+++ endpoint/user.js
@@ -8,16 +8,21 @@ var bodyParser = require('body-parser');
 
 
 /*
-    Param : full_name, register_number, student_id, class_id
+    Param : full_name, register_number, student_id, class_id, password
 */
 router.post('/user', function (req, res) {
 
     var response = {};
 
-    if(req.body.full_name && req.body.student_id && req.body.register_number && req.body.class_id){
+    if(req.body.full_name && req.body.student_id && req.body.register_number && req.body.class_id && req.body.password){
+
+      //Add to database
+      
 
     }else{
 
+      response.error = true;
+      response.message = "Field Missing";
     }
 
     res.end();
